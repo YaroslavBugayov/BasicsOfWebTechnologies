f267684f515082941bc87aac7213c2a86d0ae7b2
YaroslavBugayov <bugayov576@gmail.com>
2023-10-06 19:31:58 +0300

    login page

--- Lab5/frontend/src/app/models/response.model.ts
+++
@@ -1,7 +0,0 @@
-import {UserModel} from "./user.model";
-import {TokensModel} from "./tokens.model";
-
-export interface ResponseModel {
-  user: UserModel,
-  tokens: TokensModel
-}
--- Lab5/frontend/src/app/models/tokens.model.ts
+++
@@ -1,4 +0,0 @@
-export interface TokensModel {
-  refreshToken: string,
-  accessToken: string
-}
